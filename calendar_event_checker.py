91742f95e31ed1b2c4dc31c46988bd6af25ba443
xethrus <xethrus@gmail.com>
2023-02-02 17:06:46 -0800

    code review

--- calendar_event_checker.py
+++ calendar_event_checker.py
@@ -3,9 +3,10 @@ from icalendar import Calendar, Event
 import schedule
 import datetime
 import requests
+import time
+import json
 
 from webserver import current_json_config, status, expiration_time
-
 try:
     ics_file_name = current_json_config.get('calendar','calendar_at')
 except:
@@ -13,20 +14,22 @@ except:
     print("'calendar','calendar_at'")
 try:
     #should this be ical or ics
-    response = requests.get(ics_file_name)
+#    url = "REDACTED"
 
-    print(response)
+    ics_link_json = current_json_config.get('calendar','calendar_at')
+    ics_download_link = ics_link_json["calendar_at"]
+    response = requests.get(ics_download_link)
+    calendar = Calendar.from_ical(response.text)
 except requests.exceptions.RequestException as err:
     print("Error fetching calendar:", err)
  
 
-calendar = Calendar.from_ical(response.content)
 def check_events():
-    now = datetime.now()
+    now = datetime.datetime.now()
     for component in calendar.walk():
         if component.name == "VEVENT":
-            start = component.get("dtstart").dt
-            end = component.get("dtend").dt
+            start = component.get("dtstart").datetime
+            end = component.get("dtend").datetime
             if start <= now <= end:
                 duration = end - start
                 print("Duration:", duration)
