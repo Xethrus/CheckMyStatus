2608f69543b5171933eb27e6e05340a12b4052d9
xethrus <xethrus@gmail.com>
2023-02-13 11:39:16 -0800

    writing test and actually implementing the calendar functionality, seems to have vague functionality :D need more tests to be written

--- calendar_event_checker.py
+++ calendar_event_checker.py
@@ -11,6 +11,8 @@ import json
 
 
 from config import create_config
+config = create_config()
+
 from database_interaction_functions import modulate_status
 from database_interaction_functions import get_metadata_from_db
 
@@ -21,6 +23,20 @@ def configure_timezone_to_UTC_if_naive(unknown_datetime):
         unknown_datetime = unknown_datetime.astimezone(utc_timezone)
         return unknown_datetime
     return unknown_datetime
+
+def test_timezone_configurer():
+    naive_datetime = datetime.datetime(2023,2,13,10,30,0)
+    utc_datetime = datetime.datetime(2023,2,13,10,30,0, tzinfo=pytz.utc)
+    test_naive_to_utc = configure_timezone_to_UTC_if_naive(naive_datetime)
+    if naive_datetime != utc_datetime:
+        raise TypeError("same value, change not viable")
+    if test_naive_to_utc == utc_datetime:
+        print ("naive to utc test passed")
+    test_no_change = configure_timezone_to_UTC_if_naive(utc_datetime)
+    if test_no_change == utc_datetime:
+        print ("no change utc to utc test passed")
+    return
+
 def attempt_convert_to_datetime_if_not(dt_time):
     print("running convert datetime")
     print("dt_time:", dt_time)
@@ -28,17 +44,35 @@ def attempt_convert_to_datetime_if_not(dt_time):
         return configure_timezone_to_UTC_if_naive(dt_time)
     elif isinstance(dt_time, str):
         try:
-            date_with_time = '%Y%m%dT%H%M%S'
+            date_with_time = '%Y-%m-%d %H:%M:%S.%f %Z'
             datetime.strptime(dt_time, date_with_time)
             return configure_timezone_to_UTC_if_naive(datetime.strptime(dt_time, date_with_time))
         except ValueError:
-            print("string not matching \'%Y%m%dT%H%M%S\' format")
+            print("string not matching \'%Y-%m-%d %H:%M:%S.%f %Z\' format")
             return dt_time
     else:
         print("unsupported dt_time format for VEVENT")
         return dt_time
 
-def check_events():
+def test_convert_string_to_datetime():
+    datetime_string_utc = "2023-02-13 17:21:03.123456 UTC"
+    datetime_string_naive = "2023-02-13 17:21:03.123456" 
+    datetime_expected = datetime.datetime(2023,2,13,17,21,3,123456, tzinfo=pytz.utc)
+    datetime_utc_converted = configure_timezone_to_UTC_if_naive(datetime_string_utc)
+    datetime_naive_converted = configure_timezone_to_UTC_if_naive(datetime_string_naive)
+    
+    if datetime_utc_converted == datetime_expected:
+        print("utc datetime string to utc datetime test passed")
+    else:
+        print("utc datetime string to utc datetime test failed")
+
+    if datetime_naive_converted == datetime_expected:
+        print("naive datetime string to utc datetime test passed")
+    else:
+        print("naive datetime string to utc datetime test failed")
+
+
+def check_events(calendar, get_metadata_from_db, modulate_status):
     print("running check events")
     now = datetime.datetime.now()
     now = configure_timezone_to_UTC_if_naive(now)
@@ -75,12 +109,11 @@ def check_events():
 
 def event_thread_wrapper():
     print(" in wrap calendar thread running")
-    def event_checker_thread():
+    def event_checker_thread(config):
         print("in thread calendar thread running")
         running = True
         try:
             #issue making multi configs
-            config = create_config()
             ics_download_link = config.calendar['calendar_at']
         except:
             print("failed to retrieve from config object a:")
@@ -88,9 +121,11 @@ def event_thread_wrapper():
         try:
             response_from_ical_request = requests.get(ics_download_link)
             calendar = Calendar.from_ical(response_from_ical_request.text)
+            check_events(calendar, get_metadata_from_db, modulate_status)
         except requests.exceptions.RequestException as err:
             print("Error fetching calendar:", err)
         finally:
             time.sleep(60)
-            event_checker_thread()
-    event_checker_thread()
+            event_checker_thread(config)
+    event_checker_thread(config)
+
