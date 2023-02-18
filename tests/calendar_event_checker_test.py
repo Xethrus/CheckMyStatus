6379ec7f561b2d3ffb9e067d38b1ecda7b20ea44
xethrus <xethrus@gmail.com>
2023-02-18 09:31:42 -0800

    using unittest lib for testing calendar interaction thread components

--- tests/calendar_event_checker_test.py
+++ tests/calendar_event_checker_test.py
@@ -7,33 +7,51 @@ import pytz
 from calendar_event_checker import configure_timezone_to_UTC_if_naive
 from calendar_event_checker import attempt_convert_to_datetime_if_not
 from calendar_event_checker import check_events
+from database_interaction_functions import get_metadata_from_db
+from database_interaction_functions import modulate_status
+from config import Configuration
 
-def test_event_checker():
 
-    print("i am running the test")
+class TestEventChecker(unittest.TestCase):
 
-    test_calendar = Calendar()
-    test_calendar.add('prodid', 'test_calendar')
-    test_calendar.add('version', '2.0')
+    def setUp(self):
+        self.test_calendar = Calendar()
+        self.test_event_checker.add('prodid', 'test_calendar')
+        self.test_event_checker.add('version', '2.0')
+        self.event = Event()
+        self.event.add('summary', 'test event')
+        self.event.add('dtstart', datetime.datetime.now(pytz.utc))
+        self.event.add('dtend', dt_end)
+        self.event.add('dtstamp', datetime.datetime.now(pytz.utc))
+        self.event['uid'] = 'test-uid-123'
+        self.test_calendar.add_component(event)
+        self.connection = sqlite3.connect(':memory:')
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('''
+            CREATE TABLE savedState (
+                user TEXT PRIMARY KEY,
+                status TEXT NOT NULL,
+                expiration TEXT NOT NULL
+            );
+        ''')
+        self.cursor.execute('''
+            INSERT INTO savedState (user, status, expiration)
+            VALUES ('testuser', 'avaliable', '2025-02-22 22:22:22');
+        ''')
 
-    event = Event()
+    def tearDown(self):
+        self.test_calendar.remove_component(self.event)
+        self.test_calendar = None
+        self.connection.close()
 
-    event.add('summary', 'test event')
-    event.add('dtstart', datetime.datetime.now(pytz.utc))
-    dt_end = datetime.datetime.now() + datetime.timedelta(minutes=1)
-    event.add('dtend', dt_end)
-    event.add('dtstamp', datetime.datetime.now(pytz.utc))
-    event['uid'] = 'test-uid-123'
-
-    test_calendar.add_component(event)
-
-    print("running here lol")
-    event_found = check_events(test_calendar, get_metadata_from_db, modulate_status)
-    print("running here too lol")
-    if event_found:
-        print("event found, test found")
-    else:
-        print("event not found, test failed")
+    def test_event_checker(self):
+        event_found = False
+        config_path = '/home/xethrus/paidProject/AvaliablilityProgram/tests/test_config.ini'
+        test_config = Configuration.get_instance(config_path)
+        event_found = check_events(self.test_calendar, get_metadata_from_db, modulate_status, config_path)
+        metadata = get_metadata_from_db(self.connection, test_config)
+        self.assertEqual(self.event_found, True)
+        self.assertEqual(metadata.status, 'busy')
 
 def test_timezone_configurer():
     naive_datetime = datetime.datetime(2023,2,13,10,30,0)
@@ -66,3 +84,5 @@ def test_convert_string_to_datetime():
     else:
         print("naive datetime string to utc datetime test failed")
 
+if __name__ == '__main__':
+    unittest.main()
